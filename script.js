a6e52eb04f48b588b798407dbdfd5e3c2113fb9c
asmaa adel <asmaghlab073@gmail.com>
2024-12-01 16:42:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// //  ---------------------------------get items using id
+// const navIcon = document.getElementById('navIcon');
+// const Navlinks = document.getElementById('navLinks')
+// // ------------------------------------add even
+// navIcon.addEventListener('click',()=>{
+//     navLinks.classList.toggle('show');
+
+// // ------------change the shapeof icon when click
+//     navIcon.classList.toggle('open');
+// });
+
+
+// //..................change the shape of the icon when opening
+// navIcon.addEventListener('click',() => {
+//     navIcon.classList.toggle('open')
+//     IF (navIcon.classList.contains('open')){
+//         document.querySelectorAll('.bar').forEach((bar,index) =>{
+//             if(index === 0){
+//                 bar.Style.transform ="rotate("
+//             }
+//         })
+//     }
+// })
+
+
+function tog()
+{
+    document.body.classList.toggle('drkmode');
+}
